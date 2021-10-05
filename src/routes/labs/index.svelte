a925c5e3a232a3d69f352e4efddae3d4c4de8828
nilskj <nilskjellman@gmail.com>
2021-10-06 00:13:18 +0200

    - Change

--- src/routes/labs/index.svelte
+++ src/routes/labs/index.svelte
@@ -20,6 +20,7 @@
 	<h3 class="text-gray-400 text-xl my-2 mt-10 uppercase">Labs</h3>
 	<p>Labs are short entries of mine that I share. They could be some random rants, some code discovery or some cool experiments, enjoy!</p>
 	<ul class='mt-8'>
+
 	{#each labs as lab}
 		<li class='font-bold underline'>
 			<a href="/labs/{lab.slug}">
