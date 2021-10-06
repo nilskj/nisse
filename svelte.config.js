31670f4be97da0196c2b0fd7fa6d2e2c0d910e5b
nilskj <nilskjellman@gmail.com>
2021-10-06 10:48:53 +0200

    - Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import mdsvexConfig from './mdsvex.config.js';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -23,12 +23,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: "#svelte",
-		adapter: adapter({
-			// default options are shown
-			pages: "build",
-			assets: "build",
-			fallback: null
-		}),
+		adapter: adapter(),
 		vite: {
 			css: {
 				preprocessorOptions: {
