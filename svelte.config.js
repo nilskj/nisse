57df4e135891a2e5301e2818056c4b3fedd3c59f
nilskj <nilskjellman@gmail.com>
2022-04-19 00:55:52 +0200

    - try static

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,18 @@ const config = {
 		mdsvex(mdsvexConfig)
 	],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
